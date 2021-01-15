0a63c416a3ad20a001180f22ff2bf1c8c94fdbd2
unknown <smc5720@gmail.com>
2021-01-15 11:09:10 +0900

    20210115

---
+++ 15664.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/15664
+
+int N, M;
+int numArray[8];
+bool visited[8];
+int result[8];
+
+void DFS(int cnt, int idx)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << result[i] << " ";
+		}
+
+		cout << "\n";
+
+		return;
+	}
+
+	else
+	{
+		int xx;
+		xx = -1;
+
+		for (int i = idx; i < N; i++)
+		{
+			if (!visited[i] && xx != numArray[i])
+			{
+				visited[i] = true;
+				result[cnt] = numArray[i];
+				xx = result[cnt];
+				DFS(cnt + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> numArray[i];
+		visited[i] = false;
+	}
+
+	sort(numArray, numArray + N);
+
+	DFS(0, 0);
+
+	return 0;
+}
---
+++ 15666.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/15666
+
+int N, M;
+int numArray[8];
+int result[8];
+
+void DFS(int cnt, int idx)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << result[i] << " ";
+		}
+
+		cout << "\n";
+
+		return;
+	}
+
+	else
+	{
+		int xx;
+		xx = -1;
+
+		for (int i = idx; i < N; i++)
+		{
+			if (xx != numArray[i])
+			{
+				result[cnt] = numArray[i];
+				xx = result[cnt];
+				DFS(cnt + 1, i);
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> numArray[i];
+	}
+
+	sort(numArray, numArray + N);
+
+	DFS(0, 0);
+
+	return 0;
+}
