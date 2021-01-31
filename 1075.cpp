ead71d9e1db2a877f558b823f0e1f9b6dfacfbec
unknown <smc5720@gmail.com>
2021-01-31 15:43:20 +0900

    20210131

---
+++ 1075.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <deque>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/1075
+
+int N, F;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> F;
+
+	N -= N % 100;
+
+	int a;
+	a = N / F;
+
+	int div;
+
+	if (N % F == 0)
+	{
+		div = 0;
+	}
+
+	else
+	{
+		div = (a * F + F) % 100;
+	}
+
+	if (div < 10)
+	{
+		cout << 0;
+	}
+
+	cout << div << "\n";
+
+	return 0;
+}
