b94c2dcd67cdd71e640984870cf5dd3c43bbb767
unknown <smc5720@gmail.com>
2021-03-04 11:00:50 +0900

    20210303

---
+++ 1351.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <deque>
+#include <queue>
+#include <map>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/1351
+
+long long N, P, Q;
+map <long long, long long> m;
+
+long long func(long long n)
+{
+	if (m.count(n) > 0)
+	{
+		return m[n];
+	}
+
+	else
+	{
+		return m[n] = func(n / P) + func(n / Q);
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> P >> Q;
+
+	m[0] = 1;
+	cout << func(N) << "\n";
+
+	return 0;
+}
