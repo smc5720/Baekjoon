bab3d1098fdcbb205632900b779eb1af5ac4476e
smc5720 <smc5720@gmail.com>
2022-04-29 22:20:01 +0900

    20220429

--- Java/1719.java
+++ Java/1719.java
@@ -5,7 +5,7 @@ public class Main {
 	public static BufferedReader br;
 	public static BufferedWriter bw;
 	public static StringTokenizer st;
-	public static int n, m;
+	public static int n, m; 
 	public static int[][] dp, way;
 
 	public static void main(String[] args) throws IOException {
