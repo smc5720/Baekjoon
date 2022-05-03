b5e30575fecf6458025c7c84b5af667ea941a652
smc5720 <smc5720@gmail.com>
2022-05-03 23:02:17 +0900

    20220503

--- Java/3273.java
+++ Java/3273.java
@@ -18,7 +18,7 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
 
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
