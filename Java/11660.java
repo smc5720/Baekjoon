8c47573256f0994d91ba783d8cb4d9a32620b5e3
smc5720 <smc5720@gmail.com>
2022-09-23 21:02:35 +0900

    20220923

--- Java/11660.java
+++ Java/11660.java
@@ -1,72 +1,56 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+    public static StringTokenizer st;
+    public static int N, M;
+    public static int[][] map;
 
-	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+    /*
+        S = A - B - C + D
 
-		int[][] map = new int[N + 1][N + 1];
-		int[][] dp = new int[N + 1][N + 1];
+        - - - -     A A A -     B B B -     C - - -     D - - -
+        - S S -     A A A -     - - - -     C - - -     - - - -
+        - S S -     A A A -     - - - -     C - - -     - - - -
+        - - - -     - - - -     - - - -     - - - -     - - - -
 
-		for (int x = 0; x <= N; x++) {
-			if (x != 0) {
-				st = new StringTokenizer(br.readLine(), " ");
-			}
+        2, 2
+        3, 3        3, 3        1, 3        3, 1        1, 1
+     */
 
-			for (int y = 0; y <= N; y++) {
-				if (x == 0 || y == 0) {
-					map[x][y] = 0;
-					dp[x][y] = 0;
-					continue;
-				}
-				map[x][y] = Integer.parseInt(st.nextToken());
-				dp[x][y] = dp[x - 1][y] + dp[x][y - 1] + map[x][y] - dp[x - 1][y - 1];
-			}
-		}
-	
-		// printMap(map);
-		// printMap(dp);
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine(), " ");
 
-		for (int m = 0; m < M; m++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N + 1][N + 1];
 
-			// (x2, y2) - (x1 - 1, y2) - (x2, y1 - 1) + (x1 - 1, y1 - 1)
-			int sum = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+        for (int y = 1; y <= N; y++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-			bw.write(String.valueOf(sum));
-			bw.newLine();
-		}
+            for (int x = 1; x <= N; x++) {
+                map[y][x] = map[y - 1][x] + map[y][x - 1] - map[y - 1][x - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-	public static void printMap(int[][] map) {
-		System.out.println();
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[0].length; j++) {
-				System.out.printf("%d\t", map[i][j]);
-			}
-			System.out.println();
-		}
-	}
+            int y1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+
+            int result = map[y2][x2] - map[y1 - 1][x2] - map[y2][x1 - 1] + map[y1 - 1][x1 - 1];
+            bw.write(result + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
