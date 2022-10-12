c319e13d20eefc5df60a1d66b7028d1da3275d23
smc5720 <smc5720@gmail.com>
2022-10-12 09:47:27 +0900

    20221012

--- Java/20056.java
+++ Java/20056.java
@@ -1,192 +1,205 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	static int N, M, K;
-	static int[][] map;
-	static HashMap<Point, Queue<Fireball>> fireballs;
-	static Queue<Fireball> queue;
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+    public static StringTokenizer st;
+    public static int N, M, K;
+    public static final int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+    public static final int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+    public static Tile[][] tiles;
 
-	public static class Fireball {
-		int r, c, m, d, s;
+    public static class FireBall {
+        int r, c, m, s, d;
 
-		public Fireball(int r, int c, int m, int s, int d) {
-			this.r = r;
-			this.c = c;
-			this.m = m;
-			this.s = s;
-			this.d = d;
-		}
-	}
+        public FireBall(int r, int c, int m, int s, int d) {
+            this.r = r;
+            this.c = c;
+            this.m = m;
+            this.s = s;
+            this.d = d;
+        }
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
+        public void move() {
+            int tr = r + s * dr[d];
+            int tc = c + s * dc[d];
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+            while (tr < 0) {
+                tr += N;
+            }
 
-		fireballs = new HashMap<>();
-		queue = new LinkedList<Fireball>();
+            while (tc < 0) {
+                tc += N;
+            }
 
-		map = new int[N + 1][N + 1];
+            tr %= N;
+            tc %= N;
 
-		for (int y = 1; y <= N; y++) {
-			for (int x = 1; x <= N; x++) {
-				Point pt = new Point(x, y);
-				fireballs.put(pt, new LinkedList<Fireball>());
-			}
-		}
+            if (tr == 0) {
+                tr = N;
+            }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
+            if (tc == 0) {
+                tc = N;
+            }
 
-			Point pt = new Point(c, r);
-			fireballs.get(pt).add(new Fireball(r, c, m, s, d));
-		}
+            tiles[tr][tc].movedBalls.add(new FireBall(tr, tc, m, s, d));
+        }
+    }
 
-		// printMap();
+    public static class Tile {
+        int r, c;
+        Queue<FireBall> fireBalls, movedBalls;
 
-		for (int i = 0; i < K; i++) {
-			for (int y = 1; y <= N; y++) {
-				for (int x = 1; x <= N; x++) {
-					Point pt = new Point(x, y);
-					int size = fireballs.get(pt).size();
+        public Tile(int r, int c) {
+            this.r = r;
+            this.c = c;
+            fireBalls = new LinkedList<>();
+            movedBalls = new LinkedList<>();
+        }
 
-					for (int k = 0; k < size; k++) {
-						moveFireball(fireballs.get(pt).poll());
-					}
-				}
-			}
+        public void move() {
+            while (!fireBalls.isEmpty()) {
+                FireBall fb = fireBalls.poll();
+                fb.move();
+            }
+        }
 
-			while (!queue.isEmpty()) {
-				Fireball f = queue.poll();
-				Point pt = new Point(f.c, f.r);
-				fireballs.get(pt).add(f);
-			}
+        public void stop() {
+            fireBalls = movedBalls;
+            movedBalls = new LinkedList<>();
+        }
 
-			for (int y = 1; y <= N; y++) {
-				for (int x = 1; x <= N; x++) {
-					Point pt = new Point(x, y);
-					Queue<Fireball> q = fireballs.get(pt);
-					int size = q.size();
-					if (size > 1) {
-						// 파이어볼 질량의 합
-						int m = 0;
-						// 파이어볼 속력의 합
-						int s = 0;
-						// 첫 번째 파이어볼의 방향
-						int d = q.peek().d;
-						// 모두 방향이 홀수이거나 짝수이면 true
-						boolean state = true;
+        public void merge() {
+            if (fireBalls.size() < 2) {
+                return;
+            }
 
-						while (!q.isEmpty()) {
-							Fireball f = q.poll();
-							m += f.m;
-							s += f.s;
-							if (d % 2 != f.d % 2) {
-								state = false;
-							}
-						}
+            int tm = 0;
+            int ts = 0;
+            int cnt = fireBalls.size();
+            boolean dir = true;
+            int td = fireBalls.peek().d % 2;
 
-						m /= 5;
-						s /= size;
+            while (!fireBalls.isEmpty()) {
+                FireBall fb = fireBalls.poll();
+                tm += fb.m;
+                ts += fb.s;
 
-						if (m > 0) {
-							int[] dir = new int[4];
+                if (td != fb.d % 2) {
+                    dir = false;
+                }
+            }
 
-							if (state) {
-								for (int k = 0; k < 4; k++) {
-									dir[k] = k * 2;
-								}
-							} else {
-								for (int k = 0; k < 4; k++) {
-									dir[k] = k * 2 + 1;
-								}
-							}
+            tm /= 5;
+            ts /= cnt;
 
-							for (int k = 0; k < 4; k++) {
-								q.add(new Fireball(y, x, m, s, dir[k]));
-							}
-						}
-					}
-				}
-			}
+            if (tm == 0) {
+                return;
+            }
 
-			// printMap();
-		}
+            if (dir) {
+                fireBalls.add(new FireBall(r, c, tm, ts, 0));
+                fireBalls.add(new FireBall(r, c, tm, ts, 2));
+                fireBalls.add(new FireBall(r, c, tm, ts, 4));
+                fireBalls.add(new FireBall(r, c, tm, ts, 6));
+            } else {
+                fireBalls.add(new FireBall(r, c, tm, ts, 1));
+                fireBalls.add(new FireBall(r, c, tm, ts, 3));
+                fireBalls.add(new FireBall(r, c, tm, ts, 5));
+                fireBalls.add(new FireBall(r, c, tm, ts, 7));
+            }
+        }
 
-		int answer = 0;
+        public int getMass() {
+            int sum = 0;
 
-		for (int y = 1; y <= N; y++) {
-			for (int x = 1; x <= N; x++) {
-				Point pt = new Point(x, y);
-				Queue<Fireball> q = fireballs.get(pt);
+            for (int i = 0; i < fireBalls.size(); i++) {
+                FireBall fb = fireBalls.poll();
+                sum += fb.m;
+                fireBalls.add(fb);
+            }
 
-				while (!q.isEmpty()) {
-					answer += q.poll().m;
-				}
-			}
-		}
+            return sum;
+        }
+    }
 
-		bw.write(String.valueOf(answer));
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine(), " ");
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        tiles = new Tile[N + 1][N + 1];
 
-	public static void moveFireball(Fireball fireball) {
-		int[] dirY = { -1, -1, 0, 1, 1, 1, 0, -1 };
-		int[] dirX = { 0, 1, 1, 1, 0, -1, -1, -1 };
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                tiles[r][c] = new Tile(r, c);
+            }
+        }
 
-		// System.out.printf("(%d, %d) -> ", fireball.r, fireball.c);
-		// System.out.printf("(%d, %d) -> ", dirY[fireball.d] * fireball.s,
-		// dirX[fireball.d] * fireball.s);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-		fireball.r += dirY[fireball.d] * fireball.s;
-		fireball.c += dirX[fireball.d] * fireball.s;
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
 
-		fireball.r = computePos(fireball.r);
-		fireball.c = computePos(fireball.c);
+            FireBall fb = new FireBall(r, c, m, s, d);
+            tiles[r][c].fireBalls.add(fb);
+        }
 
-		queue.add(fireball);
+        for (int k = 0; k < K; k++) {
+            commandMove();
+            commandMerge();
+        }
 
-		// System.out.printf("(%d, %d)\n", fireball.r, fireball.c);
-	}
+        bw.write(String.valueOf(getMass()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-	public static int computePos(int pos) {
-		if (pos > N) {
-			while (pos > N) {
-				pos -= N;
-			}
-			return pos;
-		} else if (pos < 1) {
-			while (pos < 1) {
-				pos += N;
-			}
-			return pos;
-		} else {
-			return pos;
-		}
-	}
+    public static void commandMove() {
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                tiles[r][c].move();
+            }
+        }
 
-	public static void printMap() {
-		for (int y = 1; y <= N; y++) {
-			for (int x = 1; x <= N; x++) {
-				Point pt = new Point(x, y);
-				System.out.printf("%d\t", fireballs.get(pt).size());
-			}
-			System.out.println();
-		}
-	}
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                tiles[r][c].stop();
+            }
+        }
+    }
+
+    public static void commandMerge() {
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                tiles[r][c].merge();
+            }
+        }
+    }
+
+    public static int getMass() {
+        int sum = 0;
+
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                sum += tiles[r][c].getMass();
+            }
+        }
+
+        return sum;
+    }
 }
